4d9738153060275b65d19e62a8d5e1849db47cfa
Sweeper777 <sumulang@gmail.com>
2016-09-25 09:16:00 +0100

    started testing INSpriteKit

---
+++ Pods/Target Support Files/INSpriteKit/INSpriteKit-umbrella.h
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+
+#import "INSKButtonNode.h"
+#import "INSKMath.h"
+#import "INSKOSBridge.h"
+#import "INSKScrollNode.h"
+#import "INSKTiledImageNode.h"
+#import "INSKTypes.h"
+#import "INSKView.h"
+#import "INSpriteKit.h"
+#import "SKEmitterNode+INExtension.h"
+#import "SKNode+INExtension.h"
+#import "SKSpriteNode+INExtension.h"
+
+FOUNDATION_EXPORT double INSpriteKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char INSpriteKitVersionString[];
+
