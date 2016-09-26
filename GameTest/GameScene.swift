c31b0ba023345565242e7eb75a39d7a496329bb3
Sweeper777 <sumulang@gmail.com>
2016-09-26 13:37:05 +0100

    tested HLGridNode

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -1,12 +1,15 @@
 import SpriteKit
 import GameplayKit
 import INSpriteKit
+import HLSpriteKit
 
 class GameScene: SKScene {
-    private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
+        let grid = HLGridNode(gridWidth: 4, squareCount: 16, anchorPoint: CGPoint(x: 0.5, y: 0.5), layoutMode: .fill, squareSize: CGSize(width: 100, height: 100), backgroundBorderSize: 1, squareSeparatorSize: 2)
+//        grid?.backgroundColor = UIColor.white
+        (grid!.squareNode(forSquare: 0) as! HLBackdropItemNode).normalColor = UIColor.blue
+        self.addChild(grid!)
     }
     
     func buttonTapped(sender: AnyObject) {
