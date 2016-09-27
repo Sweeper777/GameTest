14f41028695b1420714abcacedd81dd401e7926c
Sweeper777 <sumulang@gmail.com>
2016-09-27 08:08:12 +0100

    tested HLLabelButtonNode

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -14,6 +14,11 @@ class GameScene: SKScene {
         swipeUpRecog = UISwipeGestureRecognizer(target: self, action: #selector(swipedUp))
         swipeUpRecog.direction = .up
         self.view!.addGestureRecognizer(swipeUpRecog)
+        let button = HLLabelButtonNode(color: UIColor.green, size: CGSize(width: 300, height: 100))!
+        button.text = "Press Me!"
+        button.fontSize = 50
+        button.position = CGPoint(x: 0, y: 250)
+        self.addChild(button)
     }
     
     func buttonTapped(sender: AnyObject) {
