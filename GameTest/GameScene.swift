91ba04b18dca343b9db4e666ab0df01d00217ea1
Sweeper777 <sumulang@gmail.com>
2016-10-01 08:26:27 +0100

    tried using HLTableLayoutManager

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -8,10 +8,17 @@ class GameScene: SKScene {
     var button: HLLabelButtonNode!
     
     override func didMove(to view: SKView) {
-        let grid = HLGridNode(gridWidth: 4, squareCount: 16, anchorPoint: CGPoint(x: 0.5, y: 0.5), layoutMode: .fill, squareSize: CGSize(width: 100, height: 100), backgroundBorderSize: 1, squareSeparatorSize: 2)
+        self.hlSetLayoutManager(HLTableLayoutManager(columnCount: 2, columnWidths: [(0.0)], columnAnchorPoints: [(0.0)], rowHeights: [(0.0)]))
+        
+        let bannerNode = SKLabelNode(text: "some text")
+        bannerNode.fontSize = 50
+        self.addChild(bannerNode)
+        
+        let grid = HLGridNode(gridWidth: 10, squareCount: 200, anchorPoint: CGPoint(x: 0.5, y: 0.5), layoutMode: .fill, squareSize: CGSize(width: 40, height: 40), backgroundBorderSize: 1, squareSeparatorSize: 2)
 //        grid?.backgroundColor = UIColor.white
         (grid!.squareNode(forSquare: 0) as! HLBackdropItemNode).normalColor = UIColor.blue
         self.addChild(grid!)
+        
         swipeUpRecog = UISwipeGestureRecognizer(target: self, action: #selector(swipedUp))
         swipeUpRecog.direction = .up
         self.view!.addGestureRecognizer(swipeUpRecog)
@@ -20,6 +27,8 @@ class GameScene: SKScene {
         button.fontSize = 50
         button.position = CGPoint(x: 0, y: 250)
         self.addChild(button)
+        
+        self.hlLayoutChildren()
     }
     
     func buttonTapped(sender: AnyObject) {
