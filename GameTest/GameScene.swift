e74e7829c7532193bd8d9be929015093f4bc4650
Sweeper777 <sumulang@gmail.com>
2016-10-17 07:12:12 +0100

    tried another usage of HLGridNode

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -8,12 +8,11 @@ class GameScene: SKScene {
         let repeating: [SKSpriteNode?] = [SKSpriteNode?](repeating: nil, count: 20)
         var colors = [[SKSpriteNode?]](repeating: repeating, count: 10)
         for i in 0..<20 {
-            colors[5][i] = SKSpriteNode(color: UIColor.white, size: CGSize(width: 20, height: 20))
+            colors[5][i] = SKSpriteNode(color: UIColor.white, size: CGSize(width: 50, height: 50))
+            self.addChild(colors[5][i]!)
         }
-        let gridLayout = HLGridLayoutManager(squareSize: CGSize(width: 20, height: 20))!
+        let gridLayout = HLGridLayoutManager(squareSize: CGSize(width: 50, height: 50))!
         gridLayout.fillMode = .downThenRight
-        self.hlSetLayoutManager(gridLayout)
         gridLayout.layoutWith2DArray(colors)
-        self.hlLayoutChildren()
     }
 }
