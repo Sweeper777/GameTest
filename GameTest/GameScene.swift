cf68ae5ad4cea3509bccb12725e0357ad73087b0
Sweeper777 <sumulang@gmail.com>
2016-10-17 07:08:29 +0100

    tested the new HLGridLayout

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -1,9 +1,19 @@
 import SpriteKit
 import GameplayKit
+import HLSpriteKit
 
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        let repeating: [SKSpriteNode?] = [SKSpriteNode?](repeating: nil, count: 20)
+        var colors = [[SKSpriteNode?]](repeating: repeating, count: 10)
+        for i in 0..<20 {
+            colors[5][i] = SKSpriteNode(color: UIColor.white, size: CGSize(width: 20, height: 20))
+        }
+        let gridLayout = HLGridLayoutManager(squareSize: CGSize(width: 20, height: 20))!
+        gridLayout.fillMode = .downThenRight
+        self.hlSetLayoutManager(gridLayout)
+        gridLayout.layoutWith2DArray(colors)
+        self.hlLayoutChildren()
     }
 }
