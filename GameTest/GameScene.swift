7fe787ad6794a40f707c7fc5d02d9842e5e585b6
Sweeper777 <sumulang@gmail.com>
2016-10-18 06:56:23 +0100

    finally got HLGridLayout working!

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -5,11 +5,11 @@ import HLSpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        let repeating: [SKSpriteNode?] = [SKSpriteNode?](repeating: nil, count: 20)
-        var colors = [[SKSpriteNode?]](repeating: repeating, count: 10)
+        let repeating: [AnyObject] = [AnyObject](repeating: NSNull(), count: 20)
+        var colors = [[AnyObject]](repeating: repeating, count: 10)
         for i in 0..<20 {
             colors[5][i] = SKSpriteNode(color: UIColor.white, size: CGSize(width: 50, height: 50))
-            self.addChild(colors[5][i]!)
+            self.addChild(colors[5][i] as! SKSpriteNode)
         }
         let gridLayout = HLGridLayoutManager(squareSize: CGSize(width: 50, height: 50))!
         gridLayout.fillMode = .downThenRight
