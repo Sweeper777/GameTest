79529050191fbfc1d1cc3f5c7a8516f50d6c7ea5
Sweeper777 <sumulang@gmail.com>
2016-10-31 07:44:43 +0000

    tested positioning sprites programmatically

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -3,5 +3,15 @@ import GameplayKit
 import HLSpriteKit
 
 class GameScene: SKScene {
-
+    override func didMove(to view: SKView) {
+        let upperLeft = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 100))
+        upperLeft.anchorPoint = CGPoint(x: 0, y: 1)
+        let point = view.convert(CGPoint.zero, to: self)
+        upperLeft.position = point
+        self.addChild(upperLeft)
+        
+        let middle = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 100))
+        middle.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.addChild(middle)
+    }
 }
