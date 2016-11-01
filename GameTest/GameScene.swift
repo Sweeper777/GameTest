b6b73f48b5105a42da3d301489ee0aaf85242063
Sweeper777 <sumulang@gmail.com>
2016-11-01 07:16:20 +0000

    tested colorizing block textures

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -6,15 +6,18 @@ class GameScene: SKScene {
     var upperLeft: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-        upperLeft = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 100))
+        upperLeft = SKSpriteNode(imageNamed: "block")
         upperLeft.anchorPoint = CGPoint(x: 0, y: 1)
         let point = view.convert(CGPoint.zero, to: self)
         upperLeft.position = point
+        upperLeft.run(SKAction.colorize(with: UIColor.red, colorBlendFactor: 0.7, duration: 0))
         self.addChild(upperLeft)
         
         let middle = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 100))
         middle.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.addChild(middle)
+        
+        self.backgroundColor = UIColor.white
     }
     
     func didChangeOrientation() {
