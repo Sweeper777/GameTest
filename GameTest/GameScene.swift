4581e65eb3d5d3f75521ecd41b95610274ad8227
Sweeper777 <sumulang@gmail.com>
2016-11-21 07:25:43 +0000

    Tested NORLabelNode

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -1,8 +1,12 @@
 import SpriteKit
 import GameplayKit
+import NORLabelNode
 
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        let label = NORLabelNode(text: "Highscore (Easy): 100\nHighscore (Normal): 100\nHighscore (Hard): 100")
+        label.fontSize = 60
+        self.addChild(label)
     }
 }
