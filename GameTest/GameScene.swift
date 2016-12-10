eef008454cccdcb3730143f1e7e70d4759e74500
Sweeper777 <sumulang@gmail.com>
2016-12-10 09:01:00 +0000

    tested whether update is called when paused

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -5,8 +5,12 @@ import NORLabelNode
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        let label = NORLabelNode(text: "Highscore (Easy): 100\nHighscore (Normal): 100\nHighscore (Hard): 100")
-        label.fontSize = 60
-        self.addChild(label)
+        let wait = SKAction.wait(forDuration: 2)
+        let pause = SKAction.run { self.isPaused = true }
+        self.run(SKAction.sequence([wait, pause]))
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        print("update")
     }
 }
