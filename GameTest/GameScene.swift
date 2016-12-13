59e6b909530a7430cb70f446128fe3c306833d65
Sweeper777 <sumulang@gmail.com>
2016-12-13 07:09:43 +0000

    tested whether background coordinate space is the same as scene coordinate space

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -4,6 +4,8 @@ import NORLabelNode
 
 class GameScene: SKScene {
     var sprite: SKSpriteNode!
+    var sprite2: SKSpriteNode!
+    var bg: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         let wait = SKAction.wait(forDuration: 2)
@@ -14,6 +16,13 @@ class GameScene: SKScene {
         sprite.position = CGPoint(x: self.frame.width / -2, y: self.frame.height / -2)
         sprite.zPosition = 1000
 //        self.addChild(sprite)
+        
+        bg = self.childNode(withName: "bg") as! SKSpriteNode
+        sprite2 = SKSpriteNode(imageNamed: "Spaceship")
+        sprite2.anchorPoint = CGPoint.zero
+        sprite2.position = CGPoint(x: self.frame.width / -2, y: self.frame.height / -2)
+        sprite2.zPosition = 1000
+        bg.addChild(sprite2)
     }
     
     override func update(_ currentTime: TimeInterval) {
