9a3103a6c5c42f7d91088f495fba0fff65b55c4c
Sweeper777 <sumulang@gmail.com>
2016-12-13 07:09:04 +0000

    tested answer to SO question

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -3,11 +3,17 @@ import GameplayKit
 import NORLabelNode
 
 class GameScene: SKScene {
+    var sprite: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         let wait = SKAction.wait(forDuration: 2)
         let pause = SKAction.run { self.isPaused = true }
         self.run(SKAction.sequence([wait, pause]))
+        sprite = SKSpriteNode(imageNamed: "Spaceship")
+        sprite.anchorPoint = CGPoint.zero
+        sprite.position = CGPoint(x: self.frame.width / -2, y: self.frame.height / -2)
+        sprite.zPosition = 1000
+//        self.addChild(sprite)
     }
     
     override func update(_ currentTime: TimeInterval) {
