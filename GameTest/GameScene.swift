bd07269e676798a17d53376a348e0b6ff117fc06
Sweeper777 <sumulang@gmail.com>
2016-12-22 07:49:29 +0800

    printed isUserInteractionEnabled

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -3,7 +3,16 @@ import GameplayKit
 import NORLabelNode
 
 class GameScene: SKScene {
+    var button: SKSpriteNode!
+    var buttonLabel: SKLabelNode!
     
     override func didMove(to view: SKView) {
+        button = childNode(withName: "newGameButton") as! SKSpriteNode
+        buttonLabel = button.childNode(withName: "buttonLabel") as! SKLabelNode
+        
+        print(button.isUserInteractionEnabled)
+        print(buttonLabel.isUserInteractionEnabled)
+        
+        print(self.isUserInteractionEnabled)
     }
 }
