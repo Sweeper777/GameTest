029609f8cf510d6c3788e517661b3cd0364410c4
Sweeper777 <sumulang@gmail.com>
2017-01-23 07:48:59 +0000

    tested running actions at the same time

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -10,11 +10,8 @@ class GameScene: SKScene {
         button = childNode(withName: "newGameButton") as! SKSpriteNode
         buttonLabel = button.childNode(withName: "buttonLabel") as! SKLabelNode
         
-        print(button.isUserInteractionEnabled)
-        print(buttonLabel.isUserInteractionEnabled)
-        
-        print(self.isUserInteractionEnabled)
-        
-        self.isUserInteractionEnabled = false
+        button.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        button.run(SKAction.sequence([SKAction.moveBy(x: 100, y: 0, duration: 3), SKAction.moveBy(x: 0, y: 100, duration: 3)]))
+        button.run(SKAction.rotate(byAngle: CGFloat(M_PI * 2), duration: 6))
     }
 }
