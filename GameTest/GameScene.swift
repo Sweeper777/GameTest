a183502f752ae97595d584ed56460607fcf9bb44
Sweeper777 <sumulang@gmail.com>
2017-01-26 06:48:52 +0000

    tested move in direction method

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -11,7 +11,29 @@ class GameScene: SKScene {
         buttonLabel = button.childNode(withName: "buttonLabel") as! SKLabelNode
         
         button.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        button.run(SKAction.sequence([SKAction.moveBy(x: 100, y: 0, duration: 3), SKAction.moveBy(x: 0, y: 100, duration: 3)]))
-        button.run(SKAction.rotate(byAngle: CGFloat(M_PI * 2), duration: 6))
+}
+
+extension SKSpriteNode {
+    func move(in direction: CGFloat) {
+        func radians(_ x: CGFloat) -> CGFloat {
+            return x * CGFloat(M_PI) / 180
+        }
+        
+        if direction < 90 && direction > 0 {
+            let dy = tan(radians(direction))
+            self.run(SKAction.moveBy(x: 200, y: dy * 200, duration: 3))
+        } else if direction < 180 {
+            let newDirection = 180 - direction
+            let dy = tan(radians(newDirection))
+            self.run(SKAction.moveBy(x: -200, y: dy * 200, duration: 3))
+        } else if direction < 270 {
+            let newDirection = direction - 180
+            let dy = -tan(radians(newDirection))
+            self.run(SKAction.moveBy(x: -200, y: dy * 200, duration: 3))
+        } else if direction < 360 {
+            let newDirection = 360 - direction
+            let dy = -tan(radians(newDirection))
+            self.run(SKAction.moveBy(x: 200, y: dy * 200, duration: 3))
+        }
     }
 }
