5a12f565b664bd67f05e03c3198d58c78db6c987
Sweeper777 <sumulang@gmail.com>
2017-03-24 06:54:06 +0000

    tested an SO question

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -3,62 +3,45 @@ import GameplayKit
 import NORLabelNode
 
 class GameScene: SKScene {
-    var button: SKSpriteNode!
-    var buttonLabel: SKLabelNode!
-    var ball1: SKSpriteNode!
+    
+    
+    var bg = SKSpriteNode()
+    
+    
+    
+    func runBgActions(bgTextureName: String) {
+        let bgTexture = SKTexture(imageNamed: bgTextureName)
+        bg.texture = bgTexture
+        bg.position = CGPoint(x: self.frame.minX, y: self.frame.minY)
+        let moveBGanimation = SKAction.move(by: CGVector(dx: 0, dy: -(bgTexture.size().height - self.frame.height)), duration: 4)
+        let shiftBGAnimation = SKAction.move(by: CGVector(dx: 0, dy: bgTexture.size().height - self.frame.height), duration: 0)
+        let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGanimation, shiftBGAnimation]))
+        bg.removeAllActions()
+        bg.run(moveBGForever)
+    }
     
     override func didMove(to view: SKView) {
-        button = childNode(withName: "newGameButton") as! SKSpriteNode
-        buttonLabel = button.childNode(withName: "buttonLabel") as! SKLabelNode
-        ball1 = childNode(withName: "ball") as! SKSpriteNode
-        button.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-    }
-    
-    func myFunction() {
-        let ball1Degrees = 315
-        if ball1Degrees < 90 {
-            ball1.physicsBody?.velocity = CGVector(dx: 1, dy: tan(Double(ball1Degrees) * (.pi / 180)))
-        }
-        if ball1Degrees == 90 {
-            ball1.physicsBody?.velocity = CGVector(dx: 1, dy: 0)
-        }
-        if ball1Degrees > 90 && ball1Degrees <= 180 {
-            ball1.physicsBody?.velocity = CGVector(dx: -1, dy: tan(Double(180 - ball1Degrees) * (.pi / 180)))
-        }
-        if ball1Degrees > 180 && ball1Degrees <= 270 {
-            ball1.physicsBody?.velocity = CGVector(dx: -1, dy: -tan(Double(ball1Degrees - 180) * (.pi / 180)))
-        }
-        if ball1Degrees > 270 && ball1Degrees <= 360 {
-            ball1.physicsBody?.velocity = CGVector(dx: 100, dy: -tan(Double(360 - ball1Degrees) * (.pi / 180)) * 100)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        myFunction()
-    }
-}
-
-extension SKSpriteNode {
-    func move(in direction: CGFloat) {
-        func radians(_ x: CGFloat) -> CGFloat {
-            return x * CGFloat(M_PI) / 180
-        }
         
-        if direction < 90 && direction > 0 {
-            let dy = tan(radians(direction))
-            self.run(SKAction.moveBy(x: 200, y: dy * 200, duration: 3))
-        } else if direction < 180 {
-            let newDirection = 180 - direction
-            let dy = tan(radians(newDirection))
-            self.run(SKAction.moveBy(x: -200, y: dy * 200, duration: 3))
-        } else if direction < 270 {
-            let newDirection = direction - 180
-            let dy = -tan(radians(newDirection))
-            self.run(SKAction.moveBy(x: -200, y: dy * 200, duration: 3))
-        } else if direction < 360 {
-            let newDirection = 360 - direction
-            let dy = -tan(radians(newDirection))
-            self.run(SKAction.moveBy(x: 200, y: dy * 200, duration: 3))
-        }
+        
+        var i: CGFloat = 0
+        
+        bg = SKSpriteNode(imageNamed: "bg1") // replace this with your first background's texture name
+        bg.position = CGPoint(x: self.frame.minX, y: self.frame.minY)
+        bg.anchorPoint = CGPoint(x: 0, y: 0)
+        bg.size.width = self.frame.width
+        bg.zPosition = -2
+        self.addChild(bg)
+        
+        i += 1
+        
+        
+        
+        let bg1Action = SKAction.run { self.runBgActions(bgTextureName: "bg1") }
+        let bg2Action = SKAction.run { self.runBgActions(bgTextureName: "bg2") }
+        let bg3Action = SKAction.run { self.runBgActions(bgTextureName: "bg3") }
+        let waitAction = SKAction.wait(forDuration: 10)
+        let sequence = SKAction.sequence([bg1Action, waitAction, bg2Action, waitAction, bg3Action, waitAction])
+        let repeatForeverAction = SKAction.repeatForever(sequence)
+        self.run(repeatForeverAction)
     }
 }
