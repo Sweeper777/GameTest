3788dd18cb7a2ab32f2227e28c0c8bcc0ad49764
Sweeper777 <sumulang@gmail.com>
2017-04-28 07:22:24 +0100

    tested action(forKey:)

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -4,7 +4,14 @@ import NORLabelNode
 
 class GameScene: SKScene {
     
+    override func didMove(to view: SKView) {
+        let action = SKAction.wait(forDuration: 3)
+        self.run(action, withKey: "myAction")
+        self.perform(#selector(test), with: nil, afterDelay: 1)
+        self.perform(#selector(test), with: nil, afterDelay: 4)
     }
     
+    func test() {
+        print(self.action(forKey: "myAction") == nil ? "Nil" : "Non-nil")
     }
 }
